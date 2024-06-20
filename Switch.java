13c28e37a6a7e6feee4a78082d9a6c48194be140
BandaruSrihitha <157792827+BandaruSrihitha@users.noreply.github.com>
2024-06-20 23:47:24 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,29 @@
+//in java we can also place string(
+import java.util.Scanner;
+class Switch{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String d=sc.next();
+        String s="Two";
+        switch(d)
+        {
+        case "one":System.out.println("Sunday");
+                   break;
+        case "two":System.out.println("Sunday");
+                   break;
+        case "three":System.out.println("Sunday");
+                   break;
+        case "four":System.out.println("Sunday");
+                   break;
+        case "five":System.out.println("Sunday");
+                   break;
+        case "six":System.out.println("Sunday");
+                   break;
+        case "seven":System.out.println("Sunday");
+                   break;
+        default:System.out.println("not a day");
+                   break;
+        }
+    }
+}
+
