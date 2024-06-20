8f37acf1c92051558f4697c169a791c5c9a26f77
BandaruSrihitha <157792827+BandaruSrihitha@users.noreply.github.com>
2024-06-20 23:53:28 +0530

    Add files via upload

---
+++ armstrong3.c
@@ -0,0 +1,33 @@
+//n armstrong  
+#include<stdio.h>
+#include<math.h>
+int armstrong();
+int armstrong(int n){
+    int sum,temp,r,d,p;
+    sum=0;
+    temp=n;
+    d=(int)log10(n)+1;
+    while(n!=0){
+        r=n%10;
+        p=(int)(pow(r,d)+0.5);
+        sum=sum+p;
+        n=n/10;
+    }
+    n=temp;
+    if(temp==sum)
+        return 1;
+    else
+        return 0;
+}
+void main(){
+    int n,i,count,m;
+    scanf("%d",&m);
+    for(i=1,count=1;count<=m;i++){
+        if(armstrong(i)){
+            if(count==m){
+                printf("%d \n",i);
+            }
+            count++;
+        }
+    }
+}
